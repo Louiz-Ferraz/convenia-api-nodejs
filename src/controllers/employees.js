e185245d831833c6d75c0981b16c3ea8797f76de
Louiz-Ferraz <luizfelipeferraz1@gmail.com>
2023-10-10 17:49:49 -0300

    Get all active employees

--- src/controllers/employees.js
+++ src/controllers/employees.js
@@ -2,7 +2,17 @@ const instanciaAxiosConvenia = require('../services/convenia');
 const knex = require('../conexao');
 
 const getAllActiveEmployees = async (req, res) => {
+  const { token } = req.headers;
 
+  try {
+    const employees = await instanciaAxiosConvenia.get(`employees`);
+    return res.status(employees.status).json(employees.data);
+  } catch (error) {
+    const { status, data: { errors } } = error.response;
+    return res.status(status).json(
+      `${errors[0].type}: ${errors[0].parameter_name} - ${errors[0].message}`
+    );
+  }
 }
 
 module.exports = {
--- src/services/convenia.js
+++ src/services/convenia.js
@@ -1,7 +1,10 @@
 const axios = require('axios');
 
 const instanciaAxiosConvenia = axios.create({
-    baseURL: 'https://public-api.convenia.com.br/api/v3/'
+    baseURL: 'https://public-api.convenia.com.br/api/v3/',
+    headers: {
+        token: process.env.CONVENIA_TOKEN
+    }
 });
 
 module.exports = instanciaAxiosConvenia;
